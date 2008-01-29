3f8fdf7b3c66be050df8fb4aa62572447292f4d3
Ulysses Bonfim <ubd06@c3sl.ufpr.br>
2008-01-29 15:25:44 -0200

    Added: Game Parser

---
+++ scripts/game/game.js
@@ -0,0 +1,55 @@
+/**
+* CHESSD - WebClient
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* C3SL - Center for Scientific Computing and Free Software
+*/
+
+
+/**
+* This file will handle all kind of game massages
+*/
+
+
+/**
+* Handle Game Messages
+*/
+function GAME_HandleGame (XML)
+{
+	var Query = XML.getElementsByTagName("query");
+	var Xmlns;
+	var Buffer = "";
+
+	// Getting query xmlns
+	if (Query.length > 0)
+	{
+		Xmlns = Query[0].getAttribute("xmlns");
+	}
+	else 
+	{
+		return "";
+	}
+
+	if (Xmlns.match(/\/chessd#game#state/))
+	{
+		Buffer = GAME_State(XML);
+	}
+}
+
+
+/**
+* Handle Game State
+*/
+function GAME_State (XML)
+{
+	return "";
+}
