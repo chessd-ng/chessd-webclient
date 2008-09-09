507160904ec7f6bdece627ebc1b5302212bd7d21
Danilo Kiyoshi Simizu Yorinori <dksy04@c3sl.ufpr.br>
2008-09-09 18:47:39 -0300

    Modified: Insert or correct GNU GPL header

--- scripts/interface/challengemenu.js
+++ scripts/interface/challengemenu.js
@@ -1,3 +1,19 @@
+/**
+* CHESSD - WebClient
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* C3SL - Center for Scientific Computing and Free Software
+*/
+
 function ChallengeMenuObj()
 {
 	var Challenge = INTERFACE_CreateChallengeMenu();
